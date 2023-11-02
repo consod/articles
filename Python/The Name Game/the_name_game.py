71ff7d1244429e51ec9ef74e783639838da67828
consod <conny-soderholm@hotmail.com>
2023-11-02 22:27:22 +0200

    Create the_name_game.py

---
+++ Python/The Name Game/the_name_game.py
@@ -0,0 +1,10 @@
+name = input()
+name2 = name[1:]
+line1 = name + ", " + name + ", bo-b" + name2
+line2 = "banana-fana fo-f" + name2
+line3 = "fee-fi-mo-m" + name2
+line4 = name + "!"
+print(line1)
+print(line2)
+print(line3)
+print(line4)
